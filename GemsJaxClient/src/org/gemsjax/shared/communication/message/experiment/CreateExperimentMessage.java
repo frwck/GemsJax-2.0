e79a2709c96a815e97b1da5c59addd496cb3d45b
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2012-04-22 16:47:31 +0200

    added Admins to CreateExperimentMessage

--- GemsJaxClient/src/org/gemsjax/shared/communication/message/experiment/CreateExperimentMessage.java
+++ GemsJaxClient/src/org/gemsjax/shared/communication/message/experiment/CreateExperimentMessage.java
@@ -12,6 +12,12 @@ import org.gemsjax.shared.experiment.ExperimentInvitation;
  * 		<new>
  * 			<name>String</name>
  * 			<desc>String</desc>
+ * 
+ * 			<admins>
+ * 				<user id="integer" />
+ * 				<user id="integer" />
+ * 				...
+ * 			</admins>
  * 			
  * 			<gr name="String" start="timeInSecond(Type=Long)" end="TimeInSeconds(Type=Long)">
  * 				<email>email@mail.com</email>
@@ -30,11 +36,8 @@ import org.gemsjax.shared.experiment.ExperimentInvitation;
  * 		</new>
  * </exp>
  * 
- * There can be arbitaray <gr> and arbitary <email> in a <gr>.
- * 
- * 
- * 
- * 
+ * There can be arbitaray <gr> and arbitary <email> in a <gr>. There is exactly one <name>, <desc> and <admins> tag.
+ * The <admins> tag contains arbitary <user> tags (including zero),
  * 
  * @author Hannes Dorfmann
  *
@@ -47,28 +50,41 @@ public class CreateExperimentMessage extends ReferenceableExperimentMessage {
 	public static final String SUBTAG_DESCRIPTION = "desc";
 	public static final String SUBTAG_GROUP ="gr";
 	public static final String SUBSUBTAG_GROUP_EMAIL="email";
-	
+	public static final String SUBTAG_ADMINS="admins";
+	public static final String SUBSUBTAG_USER = "user";
 	
 	public static final String ATTRIBUTE_GROUP_STARTDATE="start";
 	public static final String ATTRIBUTE_GROUP_ENDDATE="end";
 	public static final String ATTRIBUTE_GROUP_NAME="name";
+	public static final String ATTRIBUTE_USER_ID = "id";
 	
 	
 	private Set<ExperimentGroup> groups;
+	private Set<Integer> userIds;
 	private String name;
 	private String description;
 	
-	public CreateExperimentMessage(String referenceId, String name, String description, Set<ExperimentGroup> groups) {
+	public CreateExperimentMessage(String referenceId, String name, String description, Set<ExperimentGroup> groups, Set<Integer> userIds) {
 		super(referenceId);
 		this.name = name;
 		this.description = description;
 		this.groups = groups;
+		this.userIds = userIds;
 	}
 
 	@Override
 	public String toXml() {
 		
 		String ret = super.openingXml()+"<"+TAG+"><"+SUBTAG_NAME+">"+name+"</"+SUBTAG_NAME+"><"+SUBTAG_DESCRIPTION+">"+description+"</"+SUBTAG_DESCRIPTION+">";
+		
+		
+		ret+="<"+SUBTAG_ADMINS+">";
+		
+		for (int id : userIds)
+			ret+="<"+SUBSUBTAG_USER+" "+ATTRIBUTE_USER_ID+"=\""+id+"\" />";
+		
+		ret+="</"+SUBTAG_ADMINS+">";
+		
 		
 		for (ExperimentGroup e : groups){
 			ret+="<"+SUBTAG_GROUP+" "+ATTRIBUTE_GROUP_NAME+"=\""+name+"\" "+ATTRIBUTE_GROUP_STARTDATE+"=\""+e.getStartDate().getTime()+"\" "+ATTRIBUTE_GROUP_ENDDATE+"=\""+e.getEndDate().getTime()+"\">";
@@ -94,6 +110,12 @@ public class CreateExperimentMessage extends ReferenceableExperimentMessage {
 		return description;
 	}
 	
+	
+	public Set<Integer> getUserIds(){
+		return userIds;
+	}
+	
+	
 /*
 	public static void main(String args[]){
 		
@@ -118,4 +140,5 @@ public class CreateExperimentMessage extends ReferenceableExperimentMessage {
 		
 	}
 */
+	
 }
